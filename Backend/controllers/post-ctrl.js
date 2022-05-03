e20362e0bae168f98c3413b7f38e3ae0f160b4e1
Fabian Patras <patras.fabi@gmail.com>
2022-05-03 23:41:00 +0300

    Started the implementation of getting the posts from the backend to the HomePage

--- Backend/controllers/post-ctrl.js
+++ Backend/controllers/post-ctrl.js
@@ -1,10 +1,13 @@
-const Post = require('../models/post-model')
+// import PostModel as Post from '../models/post-model'
 
+const PostPackage = require('../models/post-model')
+const Post = PostPackage.PostModel
 
-const createPost = (req, resp) => {
+const createPost = (req, res) => {
     const body = req.body
 
     if (!body) {
+        console.log("[createPost][Error][Missing Body]")
         return res.status(400).json({
             success: false,
             error: 'You must provide a post',
@@ -14,34 +17,59 @@ const createPost = (req, resp) => {
     const post = new Post(body)
 
     if (!post) {
+        console.log("[createPost][Error][Could not create post document]")
         return res.status(400).json({
             success: false,
             error: err,
-            custom: "aici1"
         })
     }
 
-    post
-        .save()
-        .then(() => {
-            return res.status(201).json({
-                success: true,
-                id: user._id,
-                message: 'Post created!',
-            })
-        })
-        .catch(error => {
+    post.save((err, savedPost) => {
+        if (err) {
+            console.log("[createPost][Post Created][Could not save post]")
             return res.status(400).json({
-                error,
+                error: error,
                 message: 'Post not created!',
-                custom: "aici2"
             })
+        }
+
+        console.log("[createPost][Post Created][Post saved]")
+        return res.status(201).json({
+            success: true,
+            id: savedPost._id,
+            message: 'Post created!',
         })
+    })
 }
 
+const getAllPosts = (req, res) => {
+    console.log("[getAllPosts]")
+
+    Post.find({}, (err, posts) => {
+        if (err) {
+            console.log("[getAllPosts][Error][Could not get all posts]", err)
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
+        }
+
+        if (posts) {
+            console.log("[getAllPosts][Error][OK][Posts = [%O]]", posts)
+            return res.status(200).json({
+                success: true,
+                data: posts
+            })
+        }
+
+        return res
+            .status(404)
+            .json({ success: false, error: `Posts not found` })
+    })
+}
 
 module.exports = {
     createPost,
-    // getUser,
+    getAllPosts,
 }
 
--- Frontend/src/utils/components/page_content/HomePageContent.jsx
+++ Frontend/src/utils/components/page_content/HomePageContent.jsx
@@ -1,7 +1,23 @@
 import FlowerHands from '../../../assets/hands-with-flowers.png'
 import Post from '../Post'
 
+import { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 const HomePageContent = () => {
+    // var { isAuthenticated, user } = useAuth0();
+
+    // useEffect(() => {
+    //     if (!isAuthenticated) {
+    //         console.log("[PROFILE][is not Authenticated][Returning...]")
+    //         return
+    //     }
+
+    //     console.log("[PROFILE][isAuthenticated][User = %O]", user)
+    // },
+    // // eslint-disable-next-line
+    // [isAuthenticated])
+
     return (
         <div>
             <div className='flower-hands'>
